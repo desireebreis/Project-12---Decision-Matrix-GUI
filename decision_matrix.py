b1813dacc83cb82148db960e27289b30e761c798
desireebreis <desireembreis@gmail.com>
2025-10-23 13:15:48 -0600

    creating the gui

---
+++ decision_matrix.py
@@ -0,0 +1,20 @@
+# Project 12 - Decision Matrix GUI
+
+# Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tkinter as tk
+
+# def plot_matrix():
+    ''' Function to get the decision matrix based on user input '''
+
+
+# Create the main window
+root = tk.Tk()
+root.title('Project 12 - Decision Matrix')
+root.geometry('1350x600')
+root.configure(bg='#FAF3CD')
+
+# Title and logo
+logo = 
