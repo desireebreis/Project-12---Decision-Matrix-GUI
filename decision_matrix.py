231a4b91a476eead79f783911b85fe2fb5901d0e
desireebreis <desireembreis@gmail.com>
2025-11-02 22:02:06 -0700

    setting up GUI

--- decision_matrix.py
+++ decision_matrix.py
@@ -1,20 +1,26 @@
 # Project 12 - Decision Matrix GUI
 
 # Import libraries
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import tkinter as tk
+from tkinter import messagebox
+import matplotlib.pyplot as plt
 
-# def plot_matrix():
-    ''' Function to get the decision matrix based on user input '''
+class DecisionMatrix:
+    def __init__(self, root):
+        self.root = root
+
+        # Creating the GUI
+        self.root.title('Decision Matrix Generator')
+        self.root.geometry('1350x600')
+        self.root.configure(bg='#FAF3CD')
+
+        # Title and logo
+        self.logo = tk.PhotoImage(file='logo.png')
+        logo_show = tk.Label(root, image=self.logo)
 
 
-# Create the main window
-root = tk.Tk()
-root.title('Project 12 - Decision Matrix')
-root.geometry('1350x600')
-root.configure(bg='#FAF3CD')
 
-# Title and logo
-logo = 
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = DecisionMatrix(root)
+    root.mainloop()
